5ac9780e0edc1ae448628bc9df5a5afc25b04337
Abhi Rao <abhirao@gmail.com>
2013-07-14 16:14:32 -0500

    Adding mailgun config. Reorganizing config

--- Gemfile
+++ Gemfile
@@ -9,3 +9,4 @@ gem "json"
 gem "oauth2"
 gem 'sinatra-twitter-bootstrap'
 gem 'mongoid'
+gem 'rest-client'
--- app_config.rb
+++ app_config.rb
@@ -1,6 +1,8 @@
 class AppConfig
-  FILE_CONFIG  = File.exists?('config.yml') ? YAML.load_file(File.open('config.yml'))[ENV['RACK_ENV']]['foursquare'] : {}
-  FSQ_API_KEY      = FILE_CONFIG.fetch('api_key', ENV['FSQ_KEY'])
-  FSQ_API_SECRET   = FILE_CONFIG.fetch('client_secret', ENV['FSQ_SECRET'])
-  FSQ_REDIRECT_URI = FILE_CONFIG.fetch('redirect_uri', ENV['FSQ_REDIRECT_URI'])
+  FSQ_CONFIG  = File.exists?('config.yml') ? YAML.load_file(File.open('config.yml'))['foursquare'] : {}
+  MAILGUN_CONFIG  = File.exists?('config.yml') ? YAML.load_file(File.open('config.yml'))['mailgun'] : {}
+  FSQ_API_KEY      = FSQ_CONFIG.fetch('api_key', ENV['FSQ_KEY'])
+  FSQ_API_SECRET   = FSQ_CONFIG.fetch('client_secret', ENV['FSQ_SECRET'])
+  FSQ_REDIRECT_URI = FSQ_CONFIG.fetch('redirect_uri', ENV['FSQ_REDIRECT_URI'])
+  MAILGUN_API_KEY  = MAILGUN_CONFIG.fetch('api_key', ENV['MAILGUN_API_KEY'])
 end
